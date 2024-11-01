2be6bc97fdd4a46321ac639f0492e270b68af4b0
Laura Calcagni <l.calcagni@gmail.com>
2024-11-01 11:31:07 -0300

    docs: add docstrings

--- etl.py
+++ etl.py
@@ -4,6 +4,14 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads data from S3 into staging tables on Redshift.
+
+    Parameters:
+    - cur: Cursor object to execute database commands.
+    - conn: Connection object to the database.
+
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
@@ -11,6 +19,14 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """
+    Inserts data from staging tables into the final analytics tables on Redshift.
+
+    Parameters:
+    - cur: Cursor object to execute database commands.
+    - conn: Connection object to the database.
+
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
@@ -19,6 +35,13 @@ def insert_tables(cur, conn):
 
 
 def main():
+    """
+    - Establishes a connection to the Redshift database and obtains a cursor.
+    - Loads data into the staging tables.
+    - Inserts data into the final tables.
+    - Closes the database connection.
+
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
